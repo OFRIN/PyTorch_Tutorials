c112818ecdc4416bfd732fde111f89017b6c0de6
OFRIN <josanghyeokn@gmail.com>
2020-10-10 11:17:55 +0900

    update 2.1. Dataset

--- 2. DeepLearning Tutorials/2.1. Dataset/2.1.4. DataLoader/main.py
+++ 2. DeepLearning Tutorials/2.1. Dataset/2.1.4. DataLoader/main.py
@@ -4,25 +4,23 @@ import cv2
 import glob
 
 from torch.utils.data import DataLoader
+from opencv_transforms import transforms
 
-from data import Single_Classification_Dataset
-
-from augmentation.augment_utils import Chain
-from augmentation.augment_utils import Random_HorizontalFlip
-from augmentation.augment_utils import Random_ColorJitter
+from core.data import Single_Classification_Dataset
 
 if __name__ == '__main__':
     root_dir = '../../Toy_Dataset/'
 
-    transforms = Chain([
-        Random_HorizontalFlip(0.5),
-        Random_ColorJitter(brightness = 0.3, contrast = 0.3, saturation = 0.3, hue = 0.1),
+    transforms = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.ColorJitter(brightness = 0.3, contrast = 0.3, saturation = 0.3, hue = 0.1),
+        transforms.RandomHorizontalFlip(),
         lambda image: cv2.resize(image, (224, 224))
     ])
 
     dataset = Single_Classification_Dataset(root_dir, transforms, './data/train.json', ['dog', 'cat'])
-    loader = DataLoader(dataset, batch_size=16, num_workers=2, shuffle=True, pin_memory=True, drop_last=False)
-
+    loader = DataLoader(dataset, batch_size=16, num_workers=0, shuffle=True, pin_memory=True, drop_last=False)
+    
     for images, labels in loader:
 
         # torch tensor -> numpy
